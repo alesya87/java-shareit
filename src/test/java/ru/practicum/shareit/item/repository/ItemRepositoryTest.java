2eb9d39ef00d1c032ac666a0eefb03b734002059
alesyabesedina <a@ru151.ru>
2023-10-25 00:33:42 +0300

    JPATest

--- src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
